cf0aae30799443f168f9ec6b5c5858f80a9dc675
cfowl <goose_7@msn.com>
2021-02-16 20:01:47 -0700

    Chris commit to create conflict

--- app/src/main/java/com/example/phojo/MainActivity.java
+++ app/src/main/java/com/example/phojo/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 // Chris Fowler added this comment to test pushing
+// Chris Fowler's second comment to create merge conflict
 
 public class MainActivity extends AppCompatActivity {
 
