d7f9ab9b04025f8b1414d7174086ea5611beabb5
Dana <djsilvertail@byui.edu>
2021-03-02 18:54:52 -0800

    Added some shared Prefs

--- app/src/main/java/com/example/phojo/MainActivity.java
+++ app/src/main/java/com/example/phojo/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.phojo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String testString = "test real string";
+    private static final String FIRSTNAME = "";
+    private static final String LASTNAME = "";
+    private static final String EMAIL = "";
+    private static final String PASSWORD = "";
+    private static final String TAG = "";
 
     public MainActivity(Context context) {
     }
@@ -34,5 +40,9 @@ public class MainActivity extends AppCompatActivity {
     public View getActivity() {
         View view = null;
         return view;
+    }
+    protected void onClick(View view) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences ("userData", Context.MODE_PRIVATE);
     }
 }
