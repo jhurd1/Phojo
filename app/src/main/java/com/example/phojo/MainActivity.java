8316afeef1758237e5d2ee67c4c1d7cb900982ee
Jamie Hurd <jaoshu@bitbucket.org>
2021-03-02 20:51:00 -0700

    experimenting with sharedPreferences options

--- app/src/main/java/com/example/phojo/MainActivity.java
+++ app/src/main/java/com/example/phojo/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     private final String testString = "test real string";
     private static final String FIRSTNAME = "";
@@ -15,34 +17,57 @@ public class MainActivity extends AppCompatActivity {
     private static final String EMAIL = "";
     private static final String PASSWORD = "";
     private static final String TAG = "";
+    Context context = getActivity();
+    Resources res = getResources();
+    String[] userData = res.getStringArray(R.array.preference_file_key);
+    SharedPreferences sp = context.getSharedPreferences(
+            String.valueOf(res.getStringArray(R.array.preference_file_key)), Context.MODE_PRIVATE);
 
-    public MainActivity(Context context) {
+
+    public MainActivity(Context context)
+    {
     }
 
-    public MainActivity() {
+    public MainActivity()
+    {
 
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public String getTestString() {
+    public String getTestString()
+    {
         return testString;
     }
 
-    public String getHelloWorldString() {
+    public String getHelloWorldString()
+    {
         return testString;
     }
 
-    public View getActivity() {
-        View view = null;
-        return view;
+    public Context getActivity()
+    {
+        Context c = null;
+        return c;
+        /*View view = null;
+        return view;*/
     }
-    protected void onClick(View view) {
 
-        SharedPreferences sharedPreferences = getSharedPreferences ("userData", Context.MODE_PRIVATE);
+    protected void onClick(Context c) // changed to context from view
+    {
+
+        //SharedPreferences sharedPreferences = getSharedPreferences ("userData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor Edit=sp.edit();
+        Edit.putString(FIRSTNAME, "");
+        Edit.putString(LASTNAME, "");
+        Edit.putString(EMAIL, "");
+        Edit.putString(PASSWORD, "");
+        Edit.putString(TAG, "");
+
     }
 }
