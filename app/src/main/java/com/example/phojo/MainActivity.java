2bf2238cf3ea673512136dbb735373400bbecfa5
Dana <djsilvertail@byui.edu>
2021-03-03 09:49:38 -0800

    commented out the test code

--- app/src/main/java/com/example/phojo/MainActivity.java
+++ app/src/main/java/com/example/phojo/MainActivity.java
@@ -13,57 +13,6 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private final String testString = "test real string";
-    private static final String FIRSTNAME = "";
-    private static final String LASTNAME = "";
-    private static final String EMAIL = "";
-    private static final String PASSWORD = "";
-    private static final String TAG = "";
-    Context context = getActivity();
-    Resources res = getResources();
-    String[] userData = res.getStringArray(R.array.preference_file_key);
-    SharedPreferences sp = context.getSharedPreferences(
-            String.valueOf(res.getStringArray(R.array.preference_file_key)), Context.MODE_PRIVATE);
-
-
-    public MainActivity(Context context)
-    {
-    }
-
-    public MainActivity()
-    {
-
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public String getTestString()
-    {
-        return testString;
-    }
-
-    public String getHelloWorldString()
-    {
-        return testString;
-    }
-
-    public Context getActivity()
-    {
-        Context c = null;
-        return c;
-        /*View view = null;
-        return view;*/
-    }
-
-
-        SharedPreferences sharedPreferences = getSharedPreferences ("userData", Context.MODE_PRIVATE);
-    }
-
     Button bLogout;
     EditText etFirstName, etMiddleName, etLastName, etUsername;
     UserLocalStore userLocalStore;
@@ -116,8 +65,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         break;
         }
     }
-    protected void onClick(Context c) // changed to context from view
+
+}
+    /*private final String testString = "test real string";
+    private static final String FIRSTNAME = "";
+    private static final String LASTNAME = "";
+    private static final String EMAIL = "";
+    private static final String PASSWORD = "";
+    private static final String TAG = "";
+    Context context = getActivity();
+    Resources res = getResources();
+    String[] userData = res.getStringArray(R.array.preference_file_key);
+    SharedPreferences sp = context.getSharedPreferences(
+            String.valueOf(res.getStringArray(R.array.preference_file_key)), Context.MODE_PRIVATE);
+
+
+    public MainActivity(Context context)
     {
+    }
+
+    /*public MainActivity()
+    {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public String getTestString()
+    {
+        return testString;
+    }
+
+    public String getHelloWorldString()
+    {
+        return testString;
+    }
+
+    public Context getActivity()
+    {
+        Context c = null;
+        return c;
+        View view = null;
+        return view;
+    }
+
+    SharedPreferences sharedPreferences = getSharedPreferences ("userData", Context.MODE_PRIVATE);
+    }
+    protected void onClick(Context c) // changed to context from view
+        {
         //SharedPreferences sharedPreferences = getSharedPreferences ("userData", Context.MODE_PRIVATE);
         SharedPreferences.Editor Edit=sp.edit();
         Edit.putString(FIRSTNAME, ""); // replace "" with a variable holding the user's input
@@ -133,5 +133,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         edit.putString(PASSWORD, ""); // replace "" with a variable holding the user's input
         edit.putString(TAG, ""); // replace "" with a variable holding the user's input
         edit.apply();
-    }
-}
+        }*/
