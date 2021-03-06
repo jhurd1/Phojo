b32f23577b04550b892b8fbf66718f867516fa83
cfowl <goose_7@msn.com>
2021-03-05 22:55:03 -0700

    added CreateNew Activity and designed layout.

---
+++ app/src/main/java/com/example/phojo/CreateNew.java
@@ -0,0 +1,14 @@
+package com.example.phojo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new);
+    }
+}
