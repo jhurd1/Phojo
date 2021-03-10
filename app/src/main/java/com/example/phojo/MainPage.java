439aed3a4c140aa6c240cb39d970c32024de7898
Jamie Hurd <jaoshu@bitbucket.org>
2021-03-09 20:52:21 -0700

    landingPage logOut Button

--- app/src/main/java/com/example/phojo/MainPage.java
+++ app/src/main/java/com/example/phojo/MainPage.java
@@ -2,14 +2,48 @@ package com.example.phojo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainPage extends AppCompatActivity {
 
+    /************************************
+     * PRIVATE MEMBERS
+     ************************************/
+    UserLocalStore userLocalStore;
+    private static final String TAG = "landingPageLogout";
+
+    /************************************
+     * onCreate activity_landing_page
+     ************************************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
     }
+
+    /************************************
+     * onClick for LandingPage's
+     * LogOut option
+     ************************************/
+    //@Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.logoutButton:
+                userLocalStore.clearUserData();
+                userLocalStore.setUserLoggedIn(false);
+                Log.i(TAG, "LandingPage user state changed to logged out.");
+                startActivity(new Intent(this, Login.class));
+                Log.i(TAG, "Login Activity called from LandingPage.");
+                break;
+            default:
+                break;
+        }
+    }
+
 }
