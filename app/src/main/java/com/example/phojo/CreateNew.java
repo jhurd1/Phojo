46091670802523d150dcde29a13e42d18df5678c
Dana <djsilvertail@byui.edu>
2021-03-13 12:42:36 -0800

    started coding CreateNew and took out the Phojo textview

--- app/src/main/java/com/example/phojo/CreateNew.java
+++ app/src/main/java/com/example/phojo/CreateNew.java
@@ -3,12 +3,25 @@ package com.example.phojo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class CreateNew extends AppCompatActivity {
+public class CreateNew extends AppCompatActivity implements View.OnClickListener {
+
+    Button addDescription, addImages, selectCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new);
+
+        addDescription = (Button)findViewById(R.id.addDescription);
+        addImages = (Button)findViewById(R.id.addImages);
+        selectCategory = (Button)findViewById(R.id.selectCategory);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
